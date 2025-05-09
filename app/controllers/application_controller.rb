2775f485ac7eb05eaf3901c3f00704a7de5f47e7
Daniel Pierce <dlpierce@iu.edu>
2025-05-09 17:09:32 -0400

    Add missed Hyrax::Controller install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   include Hydra::Controller::ControllerBehavior
+  include Hyrax::Controller
   include Hyrax::ThemedLayoutController
   with_themed_layout '1_column'
 
