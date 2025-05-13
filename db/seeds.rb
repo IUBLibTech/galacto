4dbfc419a6a01f9f79c4c58961e3369672c7f58d
Daniel Pierce <dlpierce@iu.edu>
2025-05-13 12:09:24 -0400

    Use new style seed generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,10 @@
-ActiveFedora.fedora.connection.send(:init_base_path)
-
-puts "\n== Creating default collection types"
-Hyrax::CollectionType.find_or_create_default_collection_type
-Hyrax::CollectionType.find_or_create_admin_set_type
-
-puts "\n== Loading workflows"
-Hyrax::Workflow::WorkflowImporter.load_workflows
-errors = Hyrax::Workflow::WorkflowImporter.load_errors
-abort("Failed to process all workflows:\n  #{errors.join('\n  ')}") unless errors.empty?
-
-puts "\n== Creating default admin set"
-admin_set_id = Hyrax::AdminSetCreateService.find_or_create_default_admin_set.id.to_s
-
-# I have found that when I come back to a development
-# environment, that I may have an AdminSet in Fedora, but it is
-# not indexed in Solr.  This remediates that situation by
-# ensuring we have an indexed AdminSet
-puts "\n== Ensuring the found or created admin set is indexed"
-AdminSet.find(admin_set_id).update_index
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+Hyrax::RequiredDataSeeder.new.generate_seed_data
